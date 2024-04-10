991aef3ec2078f12e02ab6c5dceb433eb6bd7757
generyand <generyan.dep@gmail.com>
2024-04-10 16:53:23 +0800

    Last learned is how to display the data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
+  console.log(foodData);
   return (
     <div className="App">
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <FoodList foodData={foodData} />
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "34ac3495e5ba42a3b5882efd42ec84d5";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
 
   // useEffect Hook Syntax
@@ -11,7 +11,7 @@ export default function Search() {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data.results);
+      setFoodData(data.results);
     }
 
     fetchFood();
