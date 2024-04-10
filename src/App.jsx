3c29093cace7c0ccc68cc2bdd88c55906f9a2138
generyand <generyan.dep@gmail.com>
2024-04-10 17:35:07 +0800

    added a nav and styled it gamay

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
+import styles from "./App.css";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   console.log(foodData);
   return (
     <div className="App">
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <FoodList foodData={foodData} />
     </div>
